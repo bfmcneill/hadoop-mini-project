a8119f78f28f6c88abe63dcc7f379076a1c03798
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2022-09-11 13:47:02 -0600

    create script to fix file format, sample file to test shebang

---
+++ fffix.sh
@@ -0,0 +1,7 @@
+
+# to clean up issues with *.py file new lines between unix / windows run this script
+# https://stackoverflow.com/a/36885829
+
+for file in *.py ; do
+    vi +':w ++ff=unix' +':q' "${file}"
+done
--- scripts/local.sh
+++ scripts/local.sh
@@ -1,5 +1,5 @@
 cat $1 | \
-python mapper_1.py | sort | \
-python reducer_1.py | \
-python mapper_2.py | sort | \
-python reducer_2.py
+./mapper_1.py | sort | \
+./reducer_1.py | \
+./mapper_2.py | sort | \
+./reducer_2.py
