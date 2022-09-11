18102d99793ad3553241491b0e5c081f2cd58556
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2022-09-11 12:22:06 -0600

    update stream script

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -5,20 +5,24 @@ hadoop fs -copyFromLocal data/data.csv input/data.csv
 # reset HDFS directories
 hadoop fs -rm -r output
 
-hadoop-streaming-2.7.3.2.6.5.0-292.jar
-hadoop-streaming.jar
+# hadoop-streaming-2.7.3.2.6.5.0-292.jar
+# hadoop-streaming.jar
+# hadoop-*streaming*.jar
 
-hadoop \
-jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
+# step 1
+hadoop jar \
+/usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
 -files mapper_1.py,reducer_1.py \
 -mapper mapper_1.py \
 -reducer reducer_1.py \
 -input input/data.csv \
 -output output/all_accidents
 
+# step 2
 hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
--mapper ../mapper_2.py \
--reducer ../reducer_2.py \
+-files mapper_2.py,reducer_2.py \
+-mapper mapper_2.py \
+-reducer reducer_2.py \
 -input output/all_accidents \
 -output output/make_year_count
 
