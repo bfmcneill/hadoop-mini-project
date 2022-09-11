a3ec7770d494b26d1b866d06db2e91065d978b3a
Ben McNeill <bmcneill@logosresourcesllc.com>
2022-09-11 11:19:44 -0600

    update path to streaming jar

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -1,9 +1,9 @@
-hadoop jar /usr/local/hadoop/contrib/streaming/hadoop-*streaming*.jar \
--file mapper1.py -mapper mapper1.py \
--file reducer1.py -reducer reducer1.py \
+hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-*streaming*.jar \
+-file mapper_1.py -mapper mapper_1.py \
+-file reducer_1.py -reducer reducer_1.py \
 -input input/data.csv -output output/all_accidents
 
-hadoop jar /usr/local/hadoop/contrib/streaming/hadoop-*streaming*.jar \
--file mapper2.py -mapper mapper2.py \
--file reducer2.py -reducer reducer2.py \
+hadoop jar  /usr/hdp/current/hadoop-mapreduce-client/hadoop-*streaming*.jar \
+-file mapper_2.py -mapper mapper_2.py \
+-file reducer_2.py -reducer reducer_2.py \
 -input output/all_accidents -output output/make_year_count
